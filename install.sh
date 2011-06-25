961cd25bdfa9a721dcce9edfa192bfa28852fc6b
Dustin Smith <dustin@media.mit.edu>
2011-06-25 19:07:34 -0400

    install typo

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ echo "Installing dot files"
 D=`pwd`
 ln -Fs ${D}/bash_profile ../.bash_profile
 ln -Fs ${D}/bashrc ../.bashrc
-ln -Fs ${D}/aliases ../.alises
+ln -Fs ${D}/aliases ../.aliases
 ln -Fs ${D}/vimrc.local ../.vimrc.local
 
