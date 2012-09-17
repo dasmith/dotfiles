23715727c071fca0112681445a3bfc17570a368e
dustin <dustin@yuggoth.(none)>
2012-09-17 16:29:54 -0400

    updated remote host powerline

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ ln -Fs ${D}/janus.rake ../.janus.rake
 ln -Fs ${D}/matplotlibrc ../.matplotlibrc
 ln -Fs ${D}/apple ../.osx
 ln -Fs ${D}ipython_config.py ../.ipython/profile_default/ipython_config.py
+git clone -q https://github.com/milkbikis/powerline-bash
 cd powerline-bash; git pull; cd ..
 ln -Fs powerline-bash/powerline-bash.py powerline-bash.py
-git clone -q https://github.com/milkbikis/powerline-bash
 
 
